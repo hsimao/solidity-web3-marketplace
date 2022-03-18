593e42c201adaaa9c84d2337caf1498f03a29a58
Hsi Mao CHEN <hsimaochen@Hsis-MacBook-Pro.local>
2022-03-18 21:00:26 +0800

    feat: baseLayout

--- pages/index.js
+++ pages/index.js
@@ -1,25 +1,20 @@
-import { Navbar, Footer, Hero, Breadcrumbs } from "@components/common";
+import BaseLayout from "@components/layout/baseLayout";
+import { Hero, Breadcrumbs } from "@components/common";
 import { EthRates, WalletBar } from "@components/web3";
 import { OrderCard } from "@components/order";
 import { CourseList } from "@components/course";
 
 export default function Home() {
   return (
-    <div>
-      <div className="relative bg-white overflow-hidden">
-        <div className="relative max-w-7xl mx-auto px-4">
-          <Navbar />
-          <div className="fit">
-            <Hero />
-            <Breadcrumbs />
-            <EthRates />
-            <WalletBar />
-            <OrderCard />
-            <CourseList />
-          </div>
-        </div>
-        <Footer />
-      </div>
-    </div>
+    <>
+      <Hero />
+      <Breadcrumbs />
+      <EthRates />
+      <WalletBar />
+      <OrderCard />
+      <CourseList />
+    </>
   );
 }
+
+Home.Layout = BaseLayout;
