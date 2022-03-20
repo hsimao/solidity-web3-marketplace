36a6a60ac86fc3fa7143f1d1b7ed17d9379ef55e
Hsi Mao CHEN <hsimaochen@Hsis-MacBook-Pro.local>
2022-03-20 14:51:27 +0800

    feat: connect metamask account logic

--- components/ui/common/navbar.js
+++ components/ui/common/navbar.js
@@ -1,6 +1,8 @@
 import Link from "next/link";
+import { useWeb3 } from "@components/providers";
 
 export default function Navbar() {
+  const { connect } = useWeb3();
   return (
     <section>
       <div className="relative pt-6 px-4 sm:px-6 lg:px-8">
@@ -31,12 +33,12 @@ export default function Navbar() {
                   Wishlist
                 </a>
               </Link>
-              <a
-                href="#"
-                className="px-8 py-3 border rounded-md text-base font-medium text-white bg-indigo-600 hover:bg-indigo-700"
+              <span
+                onClick={connect}
+                className="cursor-pointer px-8 py-3 border rounded-md text-base font-medium text-white bg-indigo-600 hover:bg-indigo-700"
               >
                 Connect
-              </a>
+              </span>
             </div>
           </div>
         </nav>
