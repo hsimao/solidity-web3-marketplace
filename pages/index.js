9746b9b0515f9a3336c12763e51fbeae3645e138
Hsi Mao CHEN <hsimaochen@Hsis-MacBook-Pro.local>
2022-03-20 14:20:02 +0800

    feat: web3 provider

--- pages/index.js
+++ pages/index.js
@@ -2,10 +2,18 @@ import BaseLayout from "@components/ui/layout/baseLayout";
 import { Hero } from "@components/ui/common";
 import { CourseList } from "@components/ui/course";
 import { getAllCourse } from "@content/courses/fetcher";
+import { useWeb3 } from "@components/providers";
 
 export default function Home({ courses }) {
+  const { web3, isLoading } = useWeb3();
+
   return (
     <>
+      {isLoading
+        ? "Is Loading Web3"
+        : web3
+        ? "Web3 Ready!"
+        : "Plaes install metamask!"}
       <Hero />
       <CourseList courses={courses} />
     </>
