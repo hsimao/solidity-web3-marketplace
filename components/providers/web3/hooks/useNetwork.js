ef2a56a5d54411857b6f3e2be2df5e8b1465ac90
Hsi Mao CHEN <hsimaochen@Hsis-MacBook-Pro.local>
2022-03-24 20:00:32 +0800

    feat: change net id to chainId, and format display name

--- components/providers/web3/hooks/useNetwork.js
+++ components/providers/web3/hooks/useNetwork.js
@@ -1,17 +1,30 @@
 import { useEffect } from "react";
 import useSWR from "swr";
 
+const NETWORK = {
+  1: "Ethereum Main Network",
+  3: "Ropsten Test Network",
+  4: "Rinkeby Test Network",
+  5: "Goerli Test Network",
+  42: "Kovan Test Network",
+  56: "Binance Smart Chain",
+  1337: "Ganache"
+};
+
 export const handler = (web3, provider) => () => {
   const { mutate, ...rest } = useSWR(
     () => (web3 ? "web3/network" : null),
     async () => {
-      const netId = await web3.eth.net.getId();
-      return netId;
+      const chainId = await web3.eth.getChainId();
+      return NETWORK[chainId];
     }
   );
 
   useEffect(() => {
-    provider && provider.on("chainChanged", (netId) => mutate(netId));
+    provider &&
+      provider.on("chainChanged", (chainId) =>
+        mutate(NETWORK[parseInt(chainId, 16)])
+      );
   }, [provider]);
 
   return {
